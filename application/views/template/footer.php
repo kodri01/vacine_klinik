26fae293ae017f9ebdf4ca7d22f9fab3b2ded31b
kodri01 <92345975+kodri01@users.noreply.github.com>
2022-07-25 22:09:18 +0700

    Create footer.php

---
+++ application/views/template/footer.php
@@ -0,0 +1,20 @@
+<!-- <nav class="navbar fixed-bottom bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Fixed bottom</a>
+    </div>
+</nav> -->
+
+<section class="position-relative" style="margin-top: 15rem;">
+    <!-- Footer -->
+    <footer class="text-center text-white" style="background-color: #0a4275;">
+        <!-- Copyright -->
+        <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright: <label for="" class="fw-bold">Klinik Hamdalah</label>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</section>
+</body>
+
+</html>
